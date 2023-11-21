2f3eed03f26629ec4d36c578528d36e249d3b8ee
Raychaser <198485567+RaychaserRegolith@users.noreply.github.com>
2023-11-21 10:53:41 -0800

    fix some stragglers

--- src/components/calculators/ScoutingFindCalc/ScoutingFindUserList.tsx
+++ src/components/calculators/ScoutingFindCalc/ScoutingFindUserList.tsx
@@ -1,8 +1,8 @@
 import * as React from 'react'
-import { SxProps, Theme, ListItemText, ListItem, List, useTheme, ListItemAvatar, Avatar } from '@mui/material'
-import { makeAvatar, User } from '@regolithco/common'
-import { Person } from '@mui/icons-material'
+import { SxProps, Theme, ListItemText, ListItem, List, useTheme, ListItemAvatar } from '@mui/material'
+import { User } from '@regolithco/common'
 import { AppContext } from '../../../context/app.context'
+import { UserAvatar } from '../../UserAvatar'
 
 export interface ScoutingFindUserListProps {
   users: User[]
@@ -33,30 +33,15 @@ const stylesThunk = (theme: Theme): Record<string, SxProps<Theme>> => ({
 export const ScoutingFindUserList: React.FC<ScoutingFindUserListProps> = ({ users, meId, ownerId }) => {
   const theme = useTheme()
   const styles = stylesThunk(theme)
-  const { getSafeName } = React.useContext(AppContext)
+  const { getSafeName, hideNames } = React.useContext(AppContext)
 
   return (
     <List dense disablePadding sx={styles.attendanceList}>
       {users.map((user, idx) => {
-        const avtr = makeAvatar(user.avatarUrl as string)
         return (
           <ListItem key={`user-${idx}`} divider>
-            <ListItemAvatar sx={{ minWidth: 30 }}>
-              <Avatar
-                alt={user.scName}
-                src={avtr}
-                imgProps={{ referrerPolicy: 'no-referrer' }}
-                color="secondary"
-                sx={{
-                  height: 24,
-                  width: 24,
-                  background: theme.palette.secondary.main,
-                  color: theme.palette.secondary.contrastText,
-                  border: '1px solid',
-                }}
-              >
-                <Person color="inherit" />
-              </Avatar>
+            <ListItemAvatar sx={{ minWidth: 30, mr: 1 }}>
+              <UserAvatar user={user} size="small" privacy={hideNames} />
             </ListItemAvatar>
             <ListItemText primary={getSafeName(user.scName)} />
           </ListItem>
