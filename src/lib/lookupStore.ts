fd9136d7eef6db8c888ef0597b282f7acecd1790
Raychaser <198485567+RaychaserRegolith@users.noreply.github.com>
2023-12-26 16:56:13 -0500

    wiring up the endpoints

--- src/lib/lookupStore.ts
+++ src/lib/lookupStore.ts
@@ -1,13 +1,14 @@
+import React from 'react'
 import { DataStore, Lookups } from '@regolithco/common'
 import * as NodeCache from 'node-cache'
 
 // One hour cache
-export const LONG_CACHE = new NodeCache.default({ stdTTL: 60 * 60, checkperiod: 100 })
+const LONG_CACHE = new NodeCache.default({ stdTTL: 60 * 60, checkperiod: 100 })
 
 // Implement the interface for the client-side data store
 // This is the same interface as the server-side data store
 // But we get the values from the public API instead of directly
-export class ClientDataStore implements DataStore {
+class ClientDataStore implements DataStore {
   async getLookup<K extends keyof Lookups>(key: K): Promise<Lookups[K]> {
     const cached = LONG_CACHE.get(key)
     if (cached) return cached as Lookups[K]
@@ -15,4 +16,9 @@ export class ClientDataStore implements DataStore {
     const data = await response.json()
     return data as Lookups[K]
   }
+}
+
+export const useDataStore = () => {
+  const [store, setStore] = React.useState<DataStore>(new ClientDataStore())
+  return store
 }
