72d8defe9600f410a52c2f8cd852492620482727
Raychaser <198485567+RaychaserRegolith@users.noreply.github.com>
2023-12-31 16:41:39 -0500

    adding patreon button

---
+++ src/components/fields/PatreonButton.tsx
@@ -0,0 +1,19 @@
+import { Box } from '@mui/system'
+import React, { useEffect } from 'react'
+
+export const PatreonButton = () => {
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src = 'https://c6.patreon.com/becomePatronButton.bundle.js'
+    script.async = true
+    document.body.appendChild(script)
+  }, [])
+
+  return (
+    <Box sx={{ width: '11rem', border: '1px solid red' }}>
+      <a href="https://www.patreon.com/bePatron?u=64746907" data-patreon-widget-type="become-patron-button">
+        Become a member!
+      </a>
+    </Box>
+  )
+}
